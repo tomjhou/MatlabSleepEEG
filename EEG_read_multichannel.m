307247eb43a10151af836595330128d9d736a3a5
Thomas Jhou <tomjhou@gmail.com>
2022-06-22 13:07:38 -0400

    Added EEG_read_multichannel
    Added function to read multiple channels into single array.

--- EEG_read_multichannel.m
+++ EEG_read_multichannel.m
@@ -4,25 +4,18 @@
 % 
 %
 
-function [EEG_data, EMG_data, adfreq1, DateTime, OpenedFileName] = EEG_read()
+function [EEG_data, adfreq, DateTime, OpenedFileName] = EEG_read_multichannel()
 
-    EEG_CHANNEL_LEFT = 2;  % Will also accept chan 1
-    EEG_CHANNEL_RIGHT = 4; % Will also accept chan 3
-    EMG_CHANNEL = 6;       % Will also accept chan 5
-
-    % OpenedFileName = '2016-1203-1608_Box1_Ephys.plx';
-
-
-    [FileName2,PathName2,~] = uigetfile('*.plx', 'MultiSelect','on');
+    [FileName2,PathName2,FilterIndex2] = uigetfile('*.plx', 'MultiSelect','on');
     OpenedFileName = [PathName2, FileName2];
+
+    EEG_data = [];
+    adfreq = -1;
+    DateTime = [];
 
     if FileName2 == 0
         % User did not select a file
         fprintf('No file selected\n');
-        EEG_data = [];
-        EMG_data = [];
-        adfreq1 = 0;
-        DateTime = [];
         return
     end
     
@@ -45,96 +38,41 @@ function [EEG_data, EMG_data, adfreq1, DateTime, OpenedFileName] = EEG_read()
     end
     
     % get counts of # of events for spike, waveform, event, and continuous data channels
-    [tscounts, wfcounts, evcounts, continuous_counts] = plx_info(OpenedFileName,1);
+    [~, ~, ~, continuous_counts] = plx_info(OpenedFileName,1);
 
-    haveEMG = continuous_counts(EMG_CHANNEL + offset) > 0;
-    haveLeft = continuous_counts(EEG_CHANNEL_LEFT + offset) > 0;
-    haveRight = continuous_counts(EEG_CHANNEL_RIGHT + offset) > 0;
+    numPoints = -1;
 
-    if haveRight
-        % Read all data from EEG and EMG channels
-        numPoints = continuous_counts(EEG_CHANNEL_RIGHT);
-        [adfreq1, n1, EEG_data] = plx_ad_span_v(OpenedFileName, EEG_CHANNEL_RIGHT, 1, numPoints);
-    else
-        % No data in right EEG, switch to left
-        fprintf('Did not find any data in right EEG, switching to left.\n');
-        numPoints = continuous_counts(EEG_CHANNEL_LEFT);
-        [adfreq1, n1, EEG_data] = plx_ad_span_v(OpenedFileName, EEG_CHANNEL_LEFT, 1, numPoints);
-    end
-    
-    % Convert from mV to uV
-    EEG_data = EEG_data * 1000;
+    for x = 1:n
+        numPoints1 = continuous_counts(x + offset);
+        if numPoints1 == 0
+            continue
+        end
 
-    if haveEMG
-        % Find the shorter of the two waveforms (generally they should match
-        % exactly, so we're being ultra cautious here)
-        numPoints = min(numPoints, continuous_counts(EMG_CHANNEL));
+        [adfreq1, n1, EEG_data1] = plx_ad_span_v(OpenedFileName, x, 1, numPoints1);
 
-        [adfreq2, n2, EMG_data] = plx_ad_span_v(OpenedFileName, EMG_CHANNEL - offset, 1, numPoints);
-        % Convert from mV to uV
-        EMG_data = EMG_data * 1000;
-    else
-        EMG_data = [];
+        if adfreq < 0
+            adfreq = adfreq1;
+        end
+
+        if numPoints < 0
+            numPoints = numPoints1;
+        end
+
+        if adfreq1 ~= adfreq
+            fprintf('Warning: different channels has unexpected sample rate (%d instead of %d).\n', adfreq1, adfreq);
+        end
+
+        if n1 ~= numPoints
+            fprintf('Warning: channel %d has unexpected length (%d instead of %d), will skip.\n', x, numPoints1, numPoints);
+            continue;
+        end
+
+        EEG_data = [EEG_data, EEG_data1];
     end
     
     % Close all open files
     plx_close('');
-    
-    err = 0;
-    
-    if haveEMG
-        if adfreq1 ~= adfreq2
-            fprintf('Error: different sample frequencies %d and %d for EEG and EMG.\n', adfreq1, adfreq2);
-            err = 1;
-        end
-
-        if n1 ~= n2 || n1 ~= numPoints
-            fprintf('Error: different sample counts %d and %d for EEG and EMG.\n', n1, n2);
-            err = 1;
-        end
-    else
-        fprintf('Warning: no EMG data found.\n');
-    end    
-  
-    fprintf('\nSuccessfully read EEG/EMG from file %s\n', OpenedFileName);
-    fprintf('Data has %d samples, sample rate is %d Hz.\n', n1, adfreq1);
-
-    if adfreq1 >= 250
-        factor = 2;
-        if adfreq1 >= 500
-            factor = 4;
-            if (adfreq1 >= 1000)
-                factor = 8;
-            end
-        end
         
-        fprintf('Sample rate %d is higher than necessary. Downsample by %dx? ', adfreq1, factor);
-        answer = input('Enter y or n: ', 's');
-        
-        if answer == 'y' || answer == 'Y'
-            if rem(n1, factor) > 0
-                % Make sure array sizes are integer multiples of
-                % downsampling factor
-                n1 = n1 - rem(n1, factor);
-                EEG_data = EEG_data(1:n1);
-                if haveEMG
-                    n2 = n2 - rem(n2, factor);
-                    EMG_data = EMG_data(1:n2);
-                end
-            end
-            
-            % Downsample
-            EEG_data = mean(reshape(EEG_data, factor, n1/factor))';
-            if haveEMG
-                EMG_data = mean(reshape(EMG_data, factor, n1/factor))';
-            end
-        end
-        
-        adfreq1 = adfreq1 / factor;
-        
-        fprintf('Downsampled EEG/EMG from %dHz to %dHz\n', adfreq1 * factor, adfreq1);
-    end
-    
 end
 
 
